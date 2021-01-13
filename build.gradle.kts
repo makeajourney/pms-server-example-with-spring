2407689a0e12676e13aa573e0994a816339cd890
Soyoun Kim <makeajourney@gmail.com>
2021-01-14 02:24:18 +0900

    swagger 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.h2database:h2")
+
+	compile("io.springfox:springfox-swagger2:2.9.2")
+	compile("io.springfox:springfox-swagger-ui:2.9.2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ src/main/kotlin/kr/makeajourney/pms/config/SwaggerConfig.kt
@@ -0,0 +1,22 @@
+package kr.makeajourney.pms.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@EnableSwagger2
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2).select()
+            .apis(RequestHandlerSelectors.basePackage("kr.makeajourney.pms"))
+            .paths(PathSelectors.any())
+            .build();
+    }
+}
